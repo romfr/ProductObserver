dc60030d0e4a6c2275a842e9e1c08cdcdcf178b6
Ahe <info@andre-herrn.de>
2013-10-11 08:55:07 +0200

    Initial extension commit

---
+++ app/code/community/Rom/ProductObserver/Model/Resource/Setup.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Rom
+ * @package    Rom_ProductObserver
+ * @copyright  Copyright (c) 2013 ROM Agence de communication <rom@rom.fr>
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author     André Herrn <andre.herrn@rom.fr>
+ */
+
+/**
+ * Resource Setup Model
+ * 
+ * @category   Rom
+ * @package    Rom_ProductObserver
+ * @author     André Herrn <andre.herrn@rom.fr>
+ */
+class Rom_ProductObserver_Model_Resource_Setup extends Mage_Core_Model_Resource_Setup
+{ }
